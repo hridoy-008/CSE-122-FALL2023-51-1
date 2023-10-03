8345df1b27e77bffa8e64e784429b0a68e095888
Hridoy Talukder <139230807+hridoy-008@users.noreply.github.com>
2023-10-03 15:44:07 +0600

    Update Elephant.cpp

--- Lab 7/Elephant.cpp
+++ Lab 7/Elephant.cpp
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+ 
+int main() {
+    // Write C code here
+    int n,a,b,c,d,e,f,g,h,i,j;
+   scanf("%d",&n);
+   a=n/5;
+   b=n%5;
+   c=b/4;
+   d=b%4;
+   e=d/3;
+   f=d%3;
+   g=f/2;
+   h=f%2;
+   i=h/1;
+   j=h%1;
+   printf("%d",a+c+e+g+i);
+ 
+    return 0;
+}
