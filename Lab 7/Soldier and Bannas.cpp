2fa81b1f32fe1e8f9431007f32e9790a6003fd3a
Hridoy Talukder <139230807+hridoy-008@users.noreply.github.com>
2023-10-03 15:41:43 +0600

    Update Soldier and Bannas.cpp

--- Lab 7/Soldier and Bannas.cpp
+++ Lab 7/Soldier and Bannas.cpp
@@ -1,1 +1,30 @@
-
+#include <bits/stdc++.h>
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+int main()
+{
+    int k,n,w;
+    cin>>k>>n>>w;
+ 
+    int total=0;
+    for(int i=1;i<=w;i++){
+        total=total+(i*k);
+    }
+ 
+    //int d=abs(total-n);
+ 
+    if(total>n){
+        int d=abs(total-n);
+        cout<<d<<endl;
+    }
+    else{
+        cout<<"0"<<endl;
+    }
+ 
+ 
+    return 0;
+}
